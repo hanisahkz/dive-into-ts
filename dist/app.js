6ca6db677ddc17ab0721ed56bc017e0f1e46a41a
HanisahKZ <hanisahkz@gmail.com>
2018-07-02 19:10:52 +0800

    Implement mongodb in progress

--- dist/app.js
+++ dist/app.js
@@ -3,20 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
 const crmRoutes_1 = require("./routes/crmRoutes"); //naming of class will be autodetected
+const mongoose = require("mongoose");
 class App {
-    //every variable is initialized
     constructor() {
-        //todo: to understand the convention in declaring variable
         this.router = new crmRoutes_1.Routes();
+        this.mongoUrl = 'mongodb://localhost/CRMdb';
         this.app = express();
         this.config();
         this.router.routes(this.app);
+        //this.<method call defined here>
+        this.mongoSetup();
     }
     config() {
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+    mongoSetup() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 }
 exports.default = new App().app;
--- lib/app.ts
+++ lib/app.ts
@@ -1,18 +1,21 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import {Routes} from "./routes/crmRoutes"; //naming of class will be autodetected
+import * as mongoose from "mongoose";
 
 class App {
 
+    //public + varName: type = <insert relevant value>;
     public app: express.Application;
-    //todo: to understand the convention in declaring variable
     public router: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost/CRMdb';
 
-    //every variable is initialized
     constructor() {
         this.app = express();
         this.config();
         this.router.routes(this.app);
+        //this.<method call defined here>
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -20,6 +23,11 @@ class App {
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 
 }
